4986d89a1e95bbe6d0848d01877598a313f7d8b2
tommy <kazu9su@gmail.com>
2019-03-18 22:17:08 -0400

    Make actions

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,1 +1,20 @@
+import jsonfile from 'jsonfile'
 
+export const REQUEST_AGENTS = 'REQUEST_AGENTS'
+export const RECEIVE_AGENTS = 'RECEIVE_AGENTS'
+
+export const requestAgents = () => ({
+  type: REQUEST_AGENTS,
+})
+
+export const receiveAgents = json => ({
+  type: RECEIVE_AGENTS,
+  agents: json,
+})
+
+export const fetchAgents = () => (dispatch) => {
+  const file = 'src/resources/agents.json'
+  return jsonfile.readFile(file)
+    .then(obj => dispatch(receiveAgents(obj)))
+    .catch(error => console.error(error))
+}
--- src/actions/index.spec.jsx
+++ src/actions/index.spec.jsx
@@ -1,11 +1,71 @@
 import assert from 'power-assert'
-import fetchMock from 'fetch-mock'
 import thunk from 'redux-thunk'
 import configMockStore from 'redux-mock-store'
 import sinon from 'sinon'
+import jsonfile from 'jsonfile'
 
 const middleware = [thunk]
 const mockStore = configMockStore(middleware)
 
-describe('', () => {
+import { requestAgents, receiveAgents, fetchAgents } from './'
+
+describe('requestAgents', () => {
+  it(' should return REQUEST_AGENTS action', () => {
+    assert.deepStrictEqual(
+      requestAgents(),
+      {
+        type: 'REQUEST_AGENTS',
+      },
+    )
+  })
+})
+
+describe('receiveAgents', () => {
+  it('should return RECEIVE_AGENTS action', () => {
+    const agents = {
+      '1': {
+        name: 'James Milner',
+        status: 0,
+        status_updated_at: '2019-03-18T12:00Z',
+        tier: 1,
+        category: 'Medicare',
+        location: 'NY',
+        plate: 1,
+        action: [
+          'listen'
+        ],
+      }
+    }
+    assert.deepStrictEqual(
+      receiveAgents(agents),
+      {
+        type: 'RECEIVE_AGENTS',
+        agents,
+      },
+    )
+  })
+})
+
+
+describe('fetchAgents', () => {
+  it('should set agents', (done) => {
+    const store = mockStore({
+      agents: [],
+    })
+
+    store.dispatch(fetchAgents())
+      .then(() => {
+        const dispatchedActions = store.getActions()
+
+        assert.strictEqual(dispatchedActions.length, 1)
+
+        const file = 'src/resources/agents.json'
+        jsonfile.readFile(file)
+          .then(obj => {
+            assert.deepStrictEqual(dispatchedActions[0], receiveAgents(obj))
+          })
+
+          done()
+      })
+  })
 })
