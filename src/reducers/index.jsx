8688cf20122e767b736efa954daf62b64c8b24af
tommy <kazu9su@gmail.com>
2019-03-18 22:27:48 -0400

    Make reducer

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,6 +1,22 @@
 import { combineReducers } from 'redux'
+import { REQUEST_AGENTS, RECEIVE_AGENTS } from '../actions'
+
+export const agents = (state = {}, action) => {
+  switch (action.type) {
+    case REQUEST_AGENTS:
+      return state
+    case RECEIVE_AGENTS:
+      return {
+        ...state,
+        items: action.agents,
+      }
+    default:
+      return state
+  }
+}
 
 const reducers = combineReducers({
+  agents,
 })
 
 export default reducers
--- src/reducers/index.spec.jsx
+++ src/reducers/index.spec.jsx
@@ -1,4 +1,41 @@
 import assert from 'power-assert'
+import { agents } from './'
 
-describe('', () => {
+describe('agents', () => {
+  it('should return initial state', () => {
+    assert.deepStrictEqual(agents(undefined, {}), {})
+  })
+
+  it('should handle REQUEST_AGENTS action', () => {
+    const action = {
+      type: 'REQUEST_AGENTS',
+    }
+
+    assert.deepStrictEqual(agents(undefined, action), {})
+  })
+
+  it('should handle RECEIVE_AGENTS action', () => {
+    const agentsHash = {
+      '1': {
+        name: 'James Milner',
+        status: 0,
+        status_updated_at: '2019-03-18T12:00Z',
+        tier: 1,
+        category: 'Medicare',
+        location: 'NY',
+        plate: 1,
+        action: [
+          'listen'
+        ],
+      }
+    }
+    const action = {
+      type: 'RECEIVE_AGENTS',
+      agents: agentsHash,
+    }
+
+    assert.deepStrictEqual(agents(undefined, action), {
+      items: agentsHash,
+    })
+  })
 })
