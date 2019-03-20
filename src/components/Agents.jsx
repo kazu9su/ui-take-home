f03551360e7d02b2c9fe1b1994f7b813ff9a4d50
tommy <kazu9su@gmail.com>
2019-03-20 16:27:51 -0400

    Progress bar

--- src/components/Agents.jsx
+++ src/components/Agents.jsx
@@ -1,49 +1,60 @@
 import React from 'react'
 import Moment from 'react-moment'
 import PropTypes from 'prop-types'
-import { Table, TableHeader, TableHeaderColumn, TableBody, TableRow, TableRowColumn } from 'material-ui/Table'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import AgentStatusMap from './AgentStatusMap'
+
+const progress = (plate, plates) => (
+    plate / plates * 100
+)
 
 const Agents = ({ agents }) => (
   <Table>
-    <TableHeader>
+    <TableHead>
       <TableRow>
-        <TableHeaderColumn>Status</TableHeaderColumn>
-        <TableHeaderColumn>Time in State</TableHeaderColumn>
-        <TableHeaderColumn>Agent Name</TableHeaderColumn>
-        <TableHeaderColumn>Tier</TableHeaderColumn>
-        <TableHeaderColumn>Current call</TableHeaderColumn>
-        <TableHeaderColumn>Next Challenge</TableHeaderColumn>
-        <TableHeaderColumn>Actions</TableHeaderColumn>
+        <TableCell>Status</TableCell>
+        <TableCell>Time in State</TableCell>
+        <TableCell>Agent Name</TableCell>
+        <TableCell>Tier</TableCell>
+        <TableCell>Current call</TableCell>
+        <TableCell>Next Challenge</TableCell>
+        <TableCell>Actions</TableCell>
       </TableRow>
-    </TableHeader>
+    </TableHead>
     <TableBody displayRowCheckbox={false}>
       { Object.keys(agents).map(id =>
         <TableRow key={id}>
-          <TableRowColumn style={AgentStatusMap[agents[id].status].style}>
+          <TableCell style={AgentStatusMap[agents[id].status].style}>
             {AgentStatusMap[agents[id].status].label}
-          </TableRowColumn>
-          <TableRowColumn>
+          </TableCell>
+          <TableCell>
             <Moment interval={1000} date={agents[id].status_updated_at} durationFromNow>
             </Moment>
-          </TableRowColumn>
-          <TableRowColumn>
+          </TableCell>
+          <TableCell>
             {agents[id].name}
-          </TableRowColumn>
-          <TableRowColumn>
+          </TableCell>
+          <TableCell>
             {agents[id].tier}
-          </TableRowColumn>
-          <TableRowColumn>
-            {agents[id].plate}
-          </TableRowColumn>
-          <TableRowColumn>
+          </TableCell>
+          <TableCell>
+            {agents[id].status == 0 &&
+                <div>{agents[id].category} | {agents[id].location} <LinearProgress variant="determinate" value={progress(agents[id].plate, agents[id].plates)} /></div>
+            }
+          </TableCell>
+          <TableCell>
             {agents[id].plate + 1}
-          </TableRowColumn>
-          <TableRowColumn>
+          </TableCell>
+          <TableCell>
             {agents[id].actions.map((action, i) =>
               action
             )}
-          </TableRowColumn>
+          </TableCell>
         </TableRow>,
     )}
     </TableBody>
