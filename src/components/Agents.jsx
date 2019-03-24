61458ed2943527d060d37a30b1c2abe1647f7628
tommy <kazu9su@gmail.com>
2019-03-24 11:11:11 -0400

    shake icon

--- src/components/Agents.jsx
+++ src/components/Agents.jsx
@@ -10,20 +10,11 @@ import TableSortLabel from '@material-ui/core/TableSortLabel'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import agentStatusMap from './Agents/agentStatusMap'
 import headerRows from './Agents/headerRows'
+import icons from './Agents/icons'
 
 const progress = (plate, plates) => (
     plate / plates * 100
 )
-
-const icons = (action, i) => {
-  if (action == 'listen') {
-    return (
-      <svg key={i} xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 1c-4.97 0-9 4.03-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h3c1.66 0 3-1.34 3-3v-7c0-4.97-4.03-9-9-9z"/></svg>
-    )
-  } else {
-    return (<svg key={i}/>)
-  }
-}
 
 const desc = (a, b, orderBy) => {
   if (b[orderBy] < a[orderBy]) {
